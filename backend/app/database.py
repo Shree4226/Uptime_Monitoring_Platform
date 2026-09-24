2c4abb002e925b9ed3f129e5ed132870ab1d1df4
Shreya Kumari <shreesim4226@gmail.com>
2026-09-24 23:26:30 +0530

    feat: add database session

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
 from app.models import Base
 
 
 engine = create_engine(settings.database_url)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
