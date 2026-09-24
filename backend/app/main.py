754ceccb995ec6998666d8539e5d108c60f22c62
Shreya Kumari <shreesim4226@gmail.com>
2026-09-24 22:34:08 +0530

    feat: configure cors for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title=settings.app_name)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
